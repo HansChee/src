d291d407677e04745bcef65f2d25d558dee7e877
Hang Qi <qihang001@outlook.com>
2018-06-26 08:11:09 +1000

    Add project files.

---
+++ WebApi2Book.Web.Api/App_Start/WebApiConfig.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json.Serialization;
+using System.Web.Http;
+using System.Web.Http.Routing;
+using WebApi2Book.Web.Api.RouteConstraints;
+
+namespace WebApi2Book.Web.Api
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Remove XML formatter
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
+
+            // Route constraints
+            var constraintResolver = new DefaultInlineConstraintResolver();
+            constraintResolver.ConstraintMap.Add("nonzero", typeof(NonZeroConstraint));
+
+            
+            // Web API routes
+            config.MapHttpAttributeRoutes(constraintResolver);
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebApi2Book.Web.Api/Controllers/TasksController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi2Book.Web.Api.Controllers
+{
+    [RoutePrefix("api/employeeTasks")]
+    public class TasksController : ApiController
+    {
+        [Route("{id:int:max(100)}", Name = "GetTaskById")]
+        public HttpResponseMessage GetTaskWithAMaxIdOf100(int id)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.Created);
+
+            // Generate a link to the new book and set the Location header in the response.
+            string uri = Url.Link("GetTaskById", new { id });
+            response.Headers.Location = new Uri(uri);
+
+            return response;
+            //return $"In the GetTaskWithAMaxIdOf100(int id) method, id = {id}";
+        }
+
+        [Route("{id:int:min(101)}")]
+        [HttpGet]
+        public string FindTaskWithAMinIdOf101(int id)
+        {
+            return $"In the FindTaskWithAMinIdOf101(int id) method, id = {id}";
+        }
+
+        [Route("api/tasks/{taskNum:alpha}")]
+        public string Get(string taskNum)
+        {
+            return $"In the Get(string taskNum) overload, taskNum = {taskNum}";
+        }
+
+
+    }
+}
